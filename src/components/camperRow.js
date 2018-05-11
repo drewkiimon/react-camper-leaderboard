b7043f46be0bcbca749ca5b0027c95754786b069
drewkiimon <andrew.r.pagan@gmail.com>
2018-05-10 18:55:28 -0700

    Added anchor tags to each user without react redux router

--- src/components/camperRow.js
+++ src/components/camperRow.js
@@ -1,21 +1,25 @@
 import React from "react";
 import styles from "../camperRow.css";
 
+const BASE_URL = "https://www.freecodecamp.org/";
 var count = 1;
 
 const CamperRow = props => {
+  const url = `${BASE_URL}${props.username}`;
   return (
     <tr>
       <th scope="row">{count++}</th>
       <td>
-        <img
-          className="imageIcon"
-          src={props.imageUrl}
-          width="32"
-          height="32"
-          alt="image"
-        />
-        &nbsp;{props.username}
+        <a href={url} target="_blank">
+          <img
+            className="imageIcon"
+            src={props.imageUrl}
+            width="32"
+            height="32"
+            alt="image"
+          />
+          &nbsp;{props.username}
+        </a>
       </td>
       <td>{props.alltime}</td>
       <td>{props.recent}</td>
